2d7c04408c44e3486ac4627d4eb45ec3b2286608
user1 <user1@DESKTOP-MPUKNTA>
2022-12-07 12:49:03 +0900

    ADD : Day11 Wrapper, JavaApi upload
    1. Wrapper 클래스 개념 및 예제
      - Boxing, UnBoxing 예제
    2. JavaApi에서 메소드 실습
      - String, StringBuffer, StringTokenizer
      -Calendar, Date

--- src/com/kh/day10/wrapperex/Exam_Wrapper.java
+++ src/com/kh/day10/wrapperex/Exam_Wrapper.java
@@ -3,30 +3,34 @@ package com.kh.day10.wrapperex;
 public class Exam_Wrapper {
 
 	public static void main(String [] args) {
-		Integer i = Integer.valueOf(500);
-		int e = i.intValue();
+//		Integer i = Integer.valueOf(500);
+//		int e = i.intValue();
+//		
+//		Character c = Character.valueOf('c');
+//		char ch = c.charValue();
+//		
+//		Double db = Double.valueOf(3.14);
+//		double dd = db.doubleValue();
+//		
+//		// 문자열을 기본 데ㅇ이터타입으로 변환
+//		
+//		int num = Integer.parseInt("123");
+//		System.out.println("변환된 값 : " + num);
+//		
+//		boolean isyn = Boolean.parseBoolean("true");
+//		double fNum = Double.parseDouble("3.14");
+//		
+//		// 기본 데이터 타입 (int , double, char) 를 문자열로 변환
+//		
+//		String sta = Integer.toString(123);
+//		String data3 = Double.toString(3.14);
 		
-		int d = i.intValue();
+		String str1 = new String("abc");
+		str1 = "ddd";
 		
-		Integer f = null;
 		
-		Character c = Character.valueOf('c');
-		char ch = c.charValue();
+		System.out.println(str1.hashCode());
 		
-		Double db = Double.valueOf(3.14);
-		double dd = db.doubleValue();
-		
-		// 문자열을 기본 데ㅇ이터타입으로 변환
-		
-		int num = Integer.parseInt("123");
-		System.out.println("변환된 값 : " + num);
-		
-		boolean isyn = Boolean.parseBoolean("true");
-		double fNum = Double.parseDouble("3.14");
-		
-		// 기본 데이터 타입 (int , double, char) 를 문자열로 변환
-		
-		String sta = Integer.toString(123);
-		String data3 = Double.toString(3.14);
+
 	}
 }
